eee4b9cdfd16f6b45b0083eef7d8f1dc3725c6ac
Tellapati Dattaram <ramzelda861@gmail.com>
2026-06-14 17:13:32 +0530

    Time: 393 ms (31.03%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select query_name,round(sum(rating * 1.0/position)/count(query_name),2) as quality,
+round(100.0 * sum(case when rating < 3 then 1 else 0 end)/count(rating),2) as poor_query_percentage from Queries where query_name is not null group by query_name;
